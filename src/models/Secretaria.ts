a3ec1322d27b93f84043b4f256a1cdc6a242586c
caiotito <65377812+caiocalheiros@users.noreply.github.com>
2023-10-04 11:31:25 -0300

    Delete src directory

--- src/models/Secretaria.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma, Secretaria } from '@prisma/client';
-
-export const Secretarias: Prisma.Model<Secretaria> = {
-    name: 'Secretaria',
-    properties: {
-        nome: 'string',
-        RG: 'int',
-    },
-};
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.listen(3333, () => console.log("Server funcionando na porta 3333"));
